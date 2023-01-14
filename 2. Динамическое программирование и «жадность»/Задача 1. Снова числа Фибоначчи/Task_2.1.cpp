e4d3573d2982f074e3d2d4677c924979d0ddb0a6
IlyaDemidov <idemidov92@yandex.ru>
2023-01-14 14:47:14 +0300

    ДЗ № 2.1

---
+++ 2. Динамическое программирование и «жадность»/Задача 1. Снова числа Фибоначчи/Task_2.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+double fibonacci(double count)
+{
+    if (count == 0)
+        return 0; 
+    if (count == 1)
+        return 1; 
+    return fibonacci(count - 1) + fibonacci(count - 2);
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введите число: ";
+    double fiba{ 0 };
+    std::cin >> fiba;
+
+    std::cout << "Число Фибоначчи: ";
+
+    for (double count = 0; count < fiba; ++count)
+        std::cout <<fibonacci(count) << " ";
+
+          
+    
+    std::cout << "\n";
+    system("pause");
+    return 0;
+  
+}
