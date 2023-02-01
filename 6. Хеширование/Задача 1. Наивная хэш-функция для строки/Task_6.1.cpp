942dfe6de963bddc7497f4f755803758c5d82d85
IlyaDemidov <idemidov92@yandex.ru>
2023-02-02 00:02:34 +0300

    ДЗ № 6.1

---
+++ 6. Хеширование/Задача 1. Наивная хэш-функция для строки/Task_6.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int simple_string_hash(std::string& str)
+{
+    int hash{ 0 };
+    for (int i = 0; str[i]; i++)
+        hash += str[i];
+    return hash;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string str{};
+
+    while (true)
+    {
+        std::cout << "Введите строку: ";
+        std::getline(std::cin >> std::ws, str);
+
+        std::cout << "Наивный хэш строки " << str << " = " << simple_string_hash(str) << "\n";
+
+        if (str == "exit")break;
+    }
+
+    std::cout <<"\n";
+    system("pause");
+    return 0;
+}
