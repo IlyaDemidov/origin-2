a65cc7e1c874c2c0ad0ce51f5a5e45c4842bbd02
IlyaDemidov <idemidov92@yandex.ru>
2023-02-02 00:03:16 +0300

    ДЗ № 6.2

---
+++ 6. Хеширование/Задача 2. Настоящая хэш-функция для строки/Task_6.2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <cmath>
+
+long long real_string_hash(int p, int n, std::string& str)
+{
+    long long hash{ 0 };
+    for (int i = 0; str[i]; i++)
+    {
+        hash += str[i] * pow(p,i);
+        
+    }
+    return hash % n;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string str{};
+    int p{ 0 }, n{ 0 };
+
+    std::cout << "Введите p: ";
+    std::cin >> p;
+    std::cout << "Введите n: ";
+    std::cin >> n;
+
+    while (true)
+    {
+        std::cout << "Введите строку: ";
+        std::getline(std::cin >> std::ws, str);
+
+        std::cout << "Хэш строки " << str << " = " << real_string_hash(p, n, str) << "\n";
+
+        if (str == "exit")break;
+    }
+
+    std::cout <<"\n";
+    system("pause");
+    return 0;
+}
